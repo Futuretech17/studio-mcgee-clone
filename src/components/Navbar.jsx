fcfc6aff38d2d45c0d5b0d6e6e89db98e41f1a15
Benjamin <benjamisaac7@gmail.com>
2023-04-02 14:35:10 +0300

    Final navbar styling, Completed to make it responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,18 @@ import styled from 'styled-components';
 
 const Navbar = () => {
 
+    const d = new Date();
+    const year = d.getFullYear();
+
     return (
 
         
         <Container>
+        <Promotions>
+            <a href='https://www.mcgeeandco.com/pages/the-outdoor-collection' >
+                SHOP MCGEE & CO. {year} OUTDOOR COLLECTION
+            </a>
+        </Promotions>
         <Hr />  
         <Nav>
         <Logo > <Img src="https://studio-mcgee.com/app/themes/studio-mcgee/dist/images/logo_b53b4862.svg" alt=""/>      
@@ -23,6 +31,8 @@ const Navbar = () => {
         <NavLink to = "/shop"><span>Shop</span></NavLink>
         <NavLink to = "/videos"><span>Videos</span></NavLink>
         <NavLink to = "/contact"><span>Contact</span></NavLink>
+
+        <div className='logoHamburger'>
         <SearchLogo>
         <Img src="https://studio-mcgee.com/app/themes/studio-mcgee/dist/images/icon-search_1be8cefa.svg" alt=""/>
         </SearchLogo>
@@ -31,6 +41,8 @@ const Navbar = () => {
             <span></span>
             <span></span>
             <span></span>
+        </div>
+
         </div>
 
         </Navmenu>  
@@ -49,10 +61,36 @@ display:flex;
 flex-direction: column;
 justify-content:center;
 align-items: center;
-margin: 35px 0 0 0;
-background-color: #ffffff;
+margin-top: 0;
+
+
+background-color:  #f8f7f3;
+
 
 `
+
+const Promotions = styled.div`
+display:flex;
+flex-direction: column;
+justify-content:center;
+align-items:center;
+width: 100%;
+height:34px;
+
+
+
+a{
+    text-decoration: none;
+    color: #474747;
+    font-family: forma-djr-text,sans-serif;
+    font-size: 10px;
+    font-weight: 400;
+    line-height: 16px;
+    font-style: normal;
+    letter-spacing: 1.94px;
+}
+`
+
 
 const Hr = styled.hr`
 width: 100%;
@@ -66,7 +104,8 @@ display:flex;
 flex-direction:row;
 justify-content: space-between;
 width:95%;
-height: 56px;`
+height: 56px;
+background-color: #ffffff;`
 
 const Logo = styled.div`
 display:flex;
@@ -115,7 +154,24 @@ max-width:100vw;
 }  
 }
 
+.logoHamburger {
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    color:#3f3f3f;
+    font-family: forma-djr-text,sans-serif;
+    font-size: 10px;
+    font-weight: 400;
+    line-height: 16px;
+    font-style: normal;
+    letter-spacing: 1.94px;
+    @media (max-width: 992px) {
+        position:absolute;
+        right: 20px;
 
+    }
+  
 `
 
 const NavLink = styled.a` 
